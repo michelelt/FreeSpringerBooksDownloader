4e291c0da534575b18f72fb3e3826837533e1acd
michelelt <michelecocca.mc@gmail.com>
2020-05-02 22:25:09 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -13,17 +13,17 @@ if __name__== '__main__':
     if not os.path.isdir(output_path + 'pdf/'): os.mkdir(output_path+'pdf')
     if not os.path.isdir(output_path + 'epub/'): os.mkdir(output_path + 'epub')
 
-    # books_db = pd.read_excel(data_path+'BooksDB.xlsx')
-    # ml_books_db = pd.read_csv(data_path+'MLBooksDB.csv', sep=';')
-    #
-    # bcl = BookListFormatter(books_db)
-    # config = bcl.parse_config(config_path)
-    # books_to_download = bcl.select_books_to_dl(config_path, ml_books_db)
-    # list_b2d = bcl.preare_list(output_path)
-    #
-    # downloader = Downloader()
-    # for book in list_b2d:
-    #     downloader.download(book)
+    books_db = pd.read_excel(data_path+'BooksDB.xlsx')
+    ml_books_db = pd.read_csv(data_path+'MLBooksDB.csv', sep=';')
+    
+    bcl = BookListFormatter(books_db)
+    config = bcl.parse_config(config_path)
+    books_to_download = bcl.select_books_to_dl(config_path, ml_books_db)
+    list_b2d = bcl.preare_list(output_path)
+    
+    downloader = Downloader()
+    for book in list_b2d:
+        downloader.download(book)
 
 
 
